1d10f14a30b19452333d46359eddbbcafdcfe3f1
nerdparth <parthkapoor488@gmail.com>
2024-08-07 21:05:41 +0530

    added finishing touches to the frontend

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class bot_information(models.Model):
+    name=models.CharField(max_length=100, null=False, blank=False)
+    age=models.IntegerField(null=False, blank=False)
+    image = models.ImageField(upload_to='media\images')
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
